67f6df2b93f4261f8ba5e624196d146c5b6f5ac0
md naushad <kofarnine@gmail.com>
2026-07-11 19:42:58 +0530

    added search feature

--- base/views.py
+++ base/views.py
@@ -4,14 +4,23 @@ from django.urls import reverse_lazy
 from .forms import RegisterForm, CustomAuthenticationForm,ImageForm,UpdateForm
 from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 # Create your views here.
 
 
 def home(request):
-    image_data=Image.objects.all()
+    query=request.GET.get("q")
+    if query:
+        image_data=Image.objects.filter(
+            Q(catagory__name__icontains=query) | 
+            Q(title__icontains=query)
+        ).distinct()
+        print(image_data)
+    else:
+        image_data=Image.objects.all()
 
-    context={"data":image_data}
+    context={"data":image_data, "query":query}
     return render(request,"base/index.html",context)
 
 
@@ -59,6 +68,7 @@ def upload_Image(request):
             profile=form.save(commit=False)
             profile.upload_by=request.user
             profile.save()
+            form.save_m2m()
 
 
             return redirect("home")
@@ -73,13 +83,13 @@ def upload_Image(request):
 
 
 def details(request,pk):
-    image= get_object_or_404(Image, id=pk,upload_by=request.user)
+    image= get_object_or_404(Image, id=pk)
 
     context={"image":image}
     return render(request,"base/image_details.html",context)
 @login_required
 def delete_image(request,pk):
-    image=get_object_or_404(Image, id=pk)
+    image=get_object_or_404(Image, id=pk, upload_by= request.user)
 
     if request.method=="POST":
         image.delete()
@@ -88,7 +98,7 @@ def delete_image(request,pk):
     return render(request, "base/delete_image.html")
 @login_required
 def update(request,pk):
-    data=get_object_or_404(Image,id=pk)
+    data=get_object_or_404(Image,id=pk, upload_by= request.user)
     # data=Image.objects.get(id=pk, upload_by=request.user)
    
     if request.method=="POST":
